3d1ee819e260017c9f67dd8270625baf3bca2563
Greyson <sunhuitjw@gmail.com>
2018-06-29 17:24:54 +0800

    增加logbak配置 多数据源配置

--- demo/src/main/java/com/greyson/gycptest/GycpTestApplication.java
+++ demo/src/main/java/com/greyson/gycptest/GycpTestApplication.java
@@ -1,12 +1,32 @@
 package com.greyson.gycptest;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GycpTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GycpTestApplication.class, args);
+	}
+	@GetMapping("/demo")
+	public String demo() {
+		return "Hello World!!";
+	}
+	public CommandLineRunner commandLineRunner(ApplicationContext applicationContext) {
+		// 目的是
+        return args -> {
+            System.out.println("来看看 SpringBoot 默认为我们提供的 Bean：");
+            String[] beanNames = applicationContext.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            Arrays.stream(beanNames).forEach(System.out::println);
+        };
 	}
 }
