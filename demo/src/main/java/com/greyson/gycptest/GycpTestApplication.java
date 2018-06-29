b97b383a21a7707eda73cca28b0f9c801944a1bb
Greyson <sunhuitjw@gmail.com>
2018-06-29 10:40:54 +0800

    first commit

---
+++ demo/src/main/java/com/greyson/gycptest/GycpTestApplication.java
@@ -0,0 +1,12 @@
+package com.greyson.gycptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GycpTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GycpTestApplication.class, args);
+	}
+}
---
+++ demo/src/test/java/com/greyson/gycptest/GycpTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greyson.gycptest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GycpTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
